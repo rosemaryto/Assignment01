02b2262fb9781a7c9a740128fffbffcb9d050bcc
rosemaryto <rosemary_to@ymail.com>
2023-02-14 17:26:40 -0800

    init

---
+++ js/script.js
@@ -0,0 +1,168 @@
+/*
+STEP1
+let someMonth
+function theMonth()
+let currentMonth
+let summerMonth
+let myLibraryFunction
+*/
+
+/*
+STEP2
+//numeric literal
+3.9
+//string literal
+'Hello!'
+//boolean literal
+false
+//null literal
+null
+*/
+
+/*
+STEP3
+//2 complex expression
+let complEx = 3 * (2 + 5) - 1
+let complEx2 = 2
+    if (complEx2 < complEx) {
+        console.log ("Hello!")
+    }
+*/
+/*
+STEP4
+let strFirstName, strLastName, strAddress, strCity, strState, intZipCode, intYourAge, strReferralSource, blnMayWeContactYou
+*/
+
+/*
+STEP5
+let strFirstName
+strFirstName = "Rosemary"
+const strLastName = "James"
+let strCity = "San Diego"
+*/
+
+/* 
+STEP6
+let newVariable = "Rosemary" + 2
+console.log(newVariable)
+*/
+
+/* 
+STEP7
+let strNewVariable = "Rosemary" + false
+console.log(strNewVariable)
+let intYourAge = 37 + true
+console.log(intYourAge)
+*/
+
+/*
+STEP8
+yes
+let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+console.log(someString)
+*/
+
+/*
+STEP9
+//NULL
+let strPerson = null;
+console.log(strPerson)
+
+//UNDEFINED
+let strUndefined
+console.log(strUndefined)
+*/
+
+//STEP10 
+// let newString = "Hello"
+// console.log(typeof(newString))
+
+// let newInt = 5
+// console.log(typeof(newInt))
+
+// let newBln = false
+// console.log(typeof(newBln))
+
+class animal() {
+
+}
+console.log(typeof(animal))
+
+// let newThing
+// console.log(typeof(newThing))
+
+/*
+STEP11
+window.alert("Hello" + " Rosemary To" + ", welcome to the JavaScript class!")
+*/
+
+/*
+STEP12
+let name = "Rosemary To"
+window.alert("Hello " + name + ", welcome to the JavaScript class!")
+*/
+
+/*
+STEP13
+let course = "JavaScript"
+let name = "Rosemary To"
+window.alert("Hello " + name + ", welcome to the " + course + " class!")
+*/
+
+/*
+STEP14 
+let course = "JavaScript"
+let name = "Rosemary To"
+window.alert("Hello " + name + "\nWelcome to the " + course + " class!")
+*/
+
+/*
+STEP15
+let name = window.prompt("What is your name?")
+console.log(name)
+*/
+
+/*STEP16
+let course = window.prompt("What class are you taking?")
+console.log(course)
+*/
+
+/*
+STEP17
+let x = 10
+let y = 20
+let sum = x + y
+console.log(sum)
+*/
+
+/*
+STEP18
+let x = 20
+x += 20
+console.log(x)
+*/
+
+/*
+STEP19
+let x = 20
+x *= 5
+console.log(x)
+*/
+
+/*
+STEP20
+let x = 20 % 3
+console.log(x)
+*/
+
+//STEP21
+let x = 5
+if (x < 10) {
+    console.log(true)
+}
+
+//STEP22
+let y = 12
+if (y >= 4) {
+    console.log(false)
+}
